4c4dedec08a043777ac7818371a643fe3209e8fb
Neyroph <116799727+Neyroph@users.noreply.github.com>
2024-05-13 21:27:29 +0300

    adding error handling

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -8,9 +8,24 @@ function initializeSockets(io) {
     io.on("connection", (socket) =>
     {
         console.log("A user connected to the server");
+
+        socket.on("connect_error", (err) => {
+            console.log(err.message);
+            console.log(err.description);
+            console.log(err.context);
+        });
+
         onCreateRoom.initializeOnCreateRoom(socket, rooms);
         onJoinRoom.initializeOnJoinRoom(io, socket, rooms);
     });
+
+    io.engine.on("connection_error", (err) => {
+        console.log(err.req);
+        console.log(err.code);
+        console.log(err.message);
+        console.log(err.context);
+    });
+
 }
 
 module.exports = {
