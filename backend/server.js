324e664e46f7869bd3bbb986a383f587bf7ce120
Neyroph <116799727+Neyroph@users.noreply.github.com>
2024-05-13 21:33:44 +0300

    adding error handling

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const port = process.env.PORT || 8080;
-
-const server = app.listen(port)
-const io = require('socket.io').listen(server);
-
-
 
 const fs = require("fs");
 
